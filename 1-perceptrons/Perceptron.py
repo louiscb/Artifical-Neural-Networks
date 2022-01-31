f0f7613c3fb4e69dd2a61452f342b0858ca8023f
Jacob <jacmalm@kth.se>
2022-01-31 14:14:19 +0100

    perceptron learning rule sequential done

--- 1-perceptrons/Perceptron.py
+++ 1-perceptrons/Perceptron.py
@@ -1,28 +1,50 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 class PerceptronLearningRuleOnline:
 
     def __init__(self, dimensions, learning_rate):
         self.dimensions = dimensions
-        self.weights = np.random.rand((dimensions, 1))
+        self.weights = np.random.uniform(-1, 1, (dimensions, 1))
         self.learning_rate = learning_rate
 
     def iteratively_update_weights(self, training_set, labels):
         assert len(training_set) == len(labels)
+        self.visualize_predictions(training_set)
         for data_point, label in zip(training_set, labels):
             prediction = self.forward_pass(data_point)
             if prediction != label:
                 delta_w = self.learning_rate * data_point
+                delta_w = np.reshape(delta_w, (3, 1))
                 if prediction == 0:
-                    self.weights += delta_w
+                    self.weights = np.add(self.weights, delta_w)
                 else:
-                    self.weights -= delta_w
+                    self.weights = np.subtract(self.weights, delta_w)
         self.visualize_predictions(training_set)
 
     def visualize_predictions(self, training_set):
-       pass
+        class1 = []
+        class2 = []
+        for data_point in training_set:
+            if self.forward_pass(data_point) == 0:
+                class1.append(data_point)
+            else:
+                class2.append(data_point)
+        class1 = np.array(class1)
+        class2 = np.array(class2)
+        plt.scatter(class1[:, 0], class1[:, 1], c='red')
+        plt.scatter(class2[:, 0], class2[:, 1], c='blue')
+        x = np.linspace(-10, 10, 1000)
+        y = -1 * self.weights[0] / self.weights[1] * x + -1 * self.weights[2] / self.weights[1]
+        plt.plot(x, y, '-g')
+        plt.grid()
+        plt.xlim((-15, 15))
+        plt.ylim((-15, 15))
+        plt.show()
+
+
 
     def forward_pass(self, data_point):
-        prediction = np.matmul(self.weights, data_point)
+        prediction = np.matmul(self.weights.T, data_point)
         prediction = 0 if prediction < 0 else 1
         return prediction
--- 1-perceptrons/main.py
+++ 1-perceptrons/main.py
@@ -1,10 +1,14 @@
 import data_generation
+from Perceptron import PerceptronLearningRuleOnline
+import matplotlib.pyplot as plt
 
 def main():
-    d1, d2 = data_generation.generate_data_points(([-10, -10], [1, 1]), ([10, 10], [1, 1]), 100)
+    d1, d2 = data_generation.generate_data_points(([-5, -5], [1, 1]), ([5, 5], [1, 1]), 100)
     data_generation.visualize_data(d1, d2)
     data, label = data_generation.concatenate_and_shuffle(d1, d2)
     data = data_generation.add_bias(data)
+    perceptron = PerceptronLearningRuleOnline(data.shape[1], 0.1)
+    perceptron.iteratively_update_weights(data, label)
 
 
 
