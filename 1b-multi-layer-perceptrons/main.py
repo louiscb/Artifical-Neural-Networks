1273df062e7fcb89664e4b4655deffb89071e8e1
Jacob <jacmalm@kth.se>
2022-02-17 13:27:35 +0100

    fix mlp performance

--- 1b-multi-layer-perceptrons/main.py
+++ 1b-multi-layer-perceptrons/main.py
@@ -4,14 +4,12 @@ import numpy as np
 
 
 def main():
-    d1, d2 = generate_data_points([[1.0, 0.3], [0.2, 0.2]], [[0, -0.1], [0.3, 0.3]], 100)
-    data, labels = concatenate_and_shuffle(d1, d2, 0, 1)
+    d1, d2 = generate_data_points([[-3.0, -3], [1, 1]], [[3, 3], [1, 1]], 100)
+    data, labels = concatenate_and_shuffle(d1, d2, -1, 1)
     data = add_bias(data)
     model = MultiLayerPerceptron(data.shape[1], hidden_layer_size=8, output_dimensions=1, training_set=data,
-                                 learning_rate=0.0001, alpha=0.8, iterations=20)
-    #prediction = model.forward_pass()
-    #model.backward_pass(labels)
-    ratios_per_epoch = number_of_misclassified_samples_per_class(model, data, labels, epochs=200)
+                                 learning_rate=0.001, alpha=0.8)
+    ratios_per_epoch = number_of_misclassified_samples_per_class(model, data, labels, epochs=20)
     print(ratios_per_epoch)
 
 
@@ -23,12 +21,9 @@ def number_of_misclassified_samples_per_class(model, training_set, labels, epoch
         correct1 = 0
         count2 = 0
         correct2 = 0
-
         prediction = model.forward_pass()
-        index=0
+        index = 0
         for data_point, label in zip(training_set, labels):
-            
-
             if label == 0 or label == -1:
                 count1 += 1
                 if prediction[index] <= 0:
@@ -37,18 +32,12 @@ def number_of_misclassified_samples_per_class(model, training_set, labels, epoch
                 count2 += 1
                 if prediction[index] > 0:
                     correct2 += 1
-            index+=1
+            index += 1
         prediction_ratios[epoch][0] = correct1 / count1
         prediction_ratios[epoch][1] = correct2 / count2
-        total_prediction_ratio[epoch] = (correct1+correct2)/ (count1+count2)
-        if epoch==0:
-            model.first_backward_pass(labels)
-        else:
-            model.second_backward_pass(labels)
-    
-    return total_prediction_ratio
-
-
+        total_prediction_ratio[epoch] = (correct1 + correct2) / (count1 + count2)
+        model.backward_pass(labels)
+    return total_prediction_ratio, prediction_ratios
 
 
 main()
--- 1b-multi-layer-perceptrons/multi_layer_perceptron.py
+++ 1b-multi-layer-perceptrons/multi_layer_perceptron.py
@@ -1,10 +1,11 @@
 import numpy as np
 
+
 class MultiLayerPerceptron:
-    def __init__(self, input_dimensions, hidden_layer_size, output_dimensions, training_set, learning_rate, alpha, iterations):
+    def __init__(self, input_dimensions, hidden_layer_size, output_dimensions, training_set, learning_rate, alpha):
         self.h_out = None
-        self.h_in=None
-        self.o_in=None
+        self.h_in = None
+        self.o_in = None
         self.o_out = None
         self.delta_v = None
         self.delta_w = None
@@ -14,16 +15,15 @@ class MultiLayerPerceptron:
         self.training_set = training_set
         self.learning_rate = learning_rate
         self.alpha = alpha
-        self.iterations = iterations
         self.w = np.random.uniform(-1, 1, (input_dimensions, hidden_layer_size))
         self.v = np.random.uniform(-1, 1, (hidden_layer_size + 1, output_dimensions))
 
     def forward_pass(self):
         self.h_in = np.matmul(self.training_set, self.w)
         self.h_in = self.add_bias_row(self.h_in)
-        self.h_out = self.sigmoid(self.h_in)
+        self.h_out = self.phi(self.h_in)
         self.o_in = np.matmul(self.h_out, self.v)
-        self.o_out = self.sigmoid(self.o_in)
+        self.o_out = self.phi(self.o_in)
         return self.o_out
 
     def add_bias_row(self, data):
@@ -31,44 +31,28 @@ class MultiLayerPerceptron:
         bias_row = np.ones((N, 1))
         return np.concatenate((data, bias_row), axis=1)
 
-    def sigmoid(self, data):
+    def phi(self, data):
         return (2 / (np.exp(-1 * data) + 1)) - 1
 
-    def first_backward_pass(self, labels):
+    def backward_pass(self, labels):
         labels = np.reshape(labels, (len(labels), 1))
         delta_o = self.o_out - labels
-        delta_o = delta_o * self.sigmoid_prime(self.o_in)
-        delta_h = np.matmul(delta_o, self.v.T) * self.sigmoid_prime(self.h_in)
+        delta_o = delta_o * self.phi_prime(self.o_in)
+        delta_h = np.matmul(delta_o, self.v.T) * self.phi_prime(self.h_in)
+        # remove bias row
         delta_h = np.delete(delta_h, delta_h.shape[1] - 1, axis=1)
-        #remove bias row
-        #weight update
-        
-        self.delta_w = -self.learning_rate * np.matmul(self.training_set.T, delta_h)
-        self.delta_v = -self.learning_rate * np.matmul(self.h_out.T, delta_o)
 
+        # weight update
+        if self.delta_w is None:
+            self.delta_w = -self.learning_rate * np.matmul(self.training_set.T, delta_h)
+            self.delta_v = -self.learning_rate * np.matmul(self.h_out.T, delta_o)
+        else:
+            self.delta_w = self.learning_rate * (self.delta_w * self.alpha) - (1 - self.alpha) * np.matmul(
+                self.training_set.T, delta_h)
+            self.delta_v = self.learning_rate * (self.delta_v * self.alpha) - (1 - self.alpha) * np.matmul(self.h_out.T,
+                                                                                                           delta_o)
         self.w = self.w + self.delta_w
         self.v = self.v + self.delta_v
 
-    def second_backward_pass(self, labels):
-        labels = np.reshape(labels, (len(labels), 1))
-        delta_o = self.o_out - labels
-        delta_o = delta_o * self.sigmoid_prime(self.o_in)
-        delta_h = np.matmul(delta_o, self.v.T) * self.sigmoid_prime(self.h_in)
-        delta_h = np.delete(delta_h, delta_h.shape[1] - 1, axis=1)
-        #remove bias row
-        #weight update
-
-
-        self.delta_w = self.learning_rate * (self.delta_w * self.alpha) - (1 - self.alpha) * np.matmul(self.training_set.T, delta_h)
-        self.delta_v = self.learning_rate * (self.delta_v * self.alpha) - (1 - self.alpha) * np.matmul(self.h_out.T, delta_o)
-
-        self.w = self.w + self.delta_w
-        self.v = self.v + self.delta_v
-
-    def sigmoid_prime(self, input):
-        return 0.5 * (1 + self.sigmoid(input)) * (1 - self.sigmoid(input))
-
-
-
-
-    
+    def phi_prime(self, input):
+        return 0.5 * (1 + self.phi(input)) * (1 - self.phi(input))
