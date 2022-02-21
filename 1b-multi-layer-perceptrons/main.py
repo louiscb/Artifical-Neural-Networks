c1dc0cbcb42d383ebe8659d006370d2747f7110a
paddyflan <flanagp9@tcd.ie>
2022-02-21 19:10:30 +0100

    sampling

--- 1b-multi-layer-perceptrons/main.py
+++ 1b-multi-layer-perceptrons/main.py
@@ -14,45 +14,66 @@ def main():
     print(ratios_per_epoch)
 
     #sampling data
-    d1_train, d2_train, d1_val, d2_val = remove_samples(d1, d2, 100, 0.25, 0.25)
+    d1_train, d2_train, d1_val, d2_val = remove_samples(d1, d2, 100, 0.5, 0)
+    print(len(d1_train), len(d2_train), len(d1_val), len(d2_val))
     data_train, labels_train = concatenate_and_shuffle(d1_train, d2_train, -1, 1)
     data_val, labels_val = concatenate_and_shuffle(d1_val, d2_val, -1, 1)
     data_train = add_bias(data_train)
     data_val = add_bias(data_val)
     sampled_model = MultiLayerPerceptron(data_train.shape[1], hidden_layer_size=3, output_dimensions=1, training_set=data_train,
-                                learning_rate=0.001, alpha=0.8, val_set= data_val )
+                                learning_rate=0.00001, alpha=0.8, val_set= data_val )
 
-    number_of_misclassified_samples_per_class(sampled_model, data_train, labels_train, epochs=20)
-    val_prediction = sampled_model.forward_pass_val()
+    total_prediction_ratio, prediction_ratios=train_and_test_data(sampled_model, data_train, labels_train, data_val, labels_val, epochs=20)
+    print("total prediction ratio\n", total_prediction_ratio)
+    print("prediction_ratios\n",prediction_ratios )
 
 
 
-    count1 = 0
-    correct1 = 0
-    count2 = 0
-    correct2 = 0
-    index = 0
-    prediction_ratios = np.zeros(2)
-    for data_point, label in zip(data_val, labels):
-        if label == 0 or label == -1:
-            count1 += 1
-            if val_prediction[index] <= 0:
-                correct1 += 1
-        else:
-            count2 += 1
-            if val_prediction[index] > 0:
-                correct2 += 1
-        index += 1
 
-    prediction_ratios[0] = correct1 / count1
-    prediction_ratios[1] = correct2 / count2
-    total_prediction_ratio = (correct1 + correct2) / (count1 + count2)
 
-    print("classification ratio on val set is: ", prediction_ratios, total_prediction_ratio)
+
 
 
     
 
+
+
+def train_and_test_data(model, training_set, training_labels, val_set, val_labels, epochs):
+    
+    prediction_ratios = np.zeros((epochs, 2))
+    total_prediction_ratio = np.zeros(epochs)
+
+    for epoch in range(epochs):
+        count1 = 0
+        correct1 = 0
+        count2 = 0
+        correct2 = 0
+        index = 0
+        prediction = model.forward_pass_val()
+        model.forward_pass()
+        model.backward_pass(training_labels)
+
+
+        for data_point, label in zip(val_set, val_labels):
+            if label == 0 or label == -1:
+                count1 += 1
+                if prediction[index] <= 0:
+                    correct1 += 1
+            else:
+                count2 += 1
+                if prediction[index] > 0:
+                    correct2 += 1
+            index += 1
+        if count1==0:
+            prediction_ratios[epoch][0] = 0
+        else:
+            prediction_ratios[epoch][0] = correct1 / count1
+        if count2==0:
+            prediction_ratios[epoch][1] = 0
+        else:
+            prediction_ratios[epoch][1] = correct2 / count2
+        total_prediction_ratio[epoch] = (correct1 + correct2) / (count1 + count2)
+    return total_prediction_ratio, prediction_ratios
 
 
 
