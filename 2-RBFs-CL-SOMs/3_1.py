85cbd3fce0647de1b28fa954bfaeee265df2df37
paddyflan <flanagp9@tcd.ie>
2022-02-28 15:46:43 +0100

    results3.1

--- 2-RBFs-CL-SOMs/3_1.py
+++ 2-RBFs-CL-SOMs/3_1.py
@@ -1,5 +1,6 @@
 import numpy as np
 from rbf_net import RBFNetwork
+import matplotlib.pyplot as plt
 
 def sin2(x):
     return np.sin(2 * x)
@@ -10,21 +11,66 @@ def square(x):
     else:
         return -1
 
-network = RBFNetwork(n_inputs=1, n_rbf=50, n_outputs=1)
-x_train = np.arange(0, 2*np.pi, 0.1)
-y = np.zeros(x_train.shape)
-y_train_target = list(map(sin2, x_train))
 
-x_test = np.arange(0.05, 2*np.pi, 0.1)
-y_test_target = list(map(sin2, x_test))
+f = open("part1results/results3_1.txt", 'w')
+no_of_units = np.array([1,2,3,4,5,6,7,8,9,10,15,20,25,30,40,50])
+mses=np.zeros(no_of_units.shape)
 
 
+f.write("sin(2x) function:\n")
+for i in range(len(no_of_units)):
 
-network.train_leastsquaresbatch(x_train, y_train_target)
+    network = RBFNetwork(n_inputs=1, n_rbf= no_of_units[i], n_outputs=1)
+    x_train = np.arange(0, 2*np.pi, 0.1)
+    y = np.zeros(x_train.shape)
+    y_train_target = list(map(sin2, x_train))
 
-mse= network.test_leastsquaresbatch(x_test, y_test_target)
+    x_test = np.arange(0.05, 2*np.pi, 0.1)
+    y_test_target = list(map(sin2, x_test))
 
-print(mse)
+
+
+    network.train_leastsquaresbatch(x_train, y_train_target)
+
+    mse= network.test_leastsquaresbatch(x_test, y_test_target)
+    mses[i] = mse
+    
+    f.write("no. units: " + str(no_of_units[i]) + "  mse: " + str(mses[i]) + '\n')
+
+
+
+
+
+
+
+no_of_units = np.array([1,2,3,4,5,6,7,8,9,10,15,20,25,30,40,50])
+mses=np.zeros(no_of_units.shape)
+
+f.write("\nsquare(2x) function:\n")
+for i in range(len(no_of_units)):
+
+    network2 = RBFNetwork(n_inputs=1, n_rbf= no_of_units[i], n_outputs=1)
+    x_train = np.arange(0, 2*np.pi, 0.1)
+    y = np.zeros(x_train.shape)
+    y_train_target = list(map(square, x_train))
+
+    x_test = np.arange(0.05, 2*np.pi, 0.1)
+    y_test_target = list(map(square, x_test))
+
+
+
+    network2.train_leastsquaresbatch(x_train, y_train_target)
+
+    mse= network2.test_leastsquaresbatch(x_test, y_test_target)
+    mses[i] = mse
+    
+    f.write("no. units: " + str(no_of_units[i]) + "  mse: " + str(mses[i]) + '\n')
+
+
+
+
+
+
 
 
 
