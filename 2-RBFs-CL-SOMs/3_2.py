751fba6b1e31a983db19d51e2eac2c04a0db523f
paddyflan <flanagp9@tcd.ie>
2022-03-01 14:56:27 +0100

    changing learningrates

--- 2-RBFs-CL-SOMs/3_2.py
+++ 2-RBFs-CL-SOMs/3_2.py
@@ -17,14 +17,15 @@ def add_noise(data, variance):
     return data
 
 
-f = open("part1results/results3_2.txt", 'w')
-f.write('\nwidth=0.1\n')
-no_of_units = np.array([1,2,3,4,5,6,7,8,9,10,15,20,25,30,40,50,100])
-mses=np.zeros(no_of_units.shape)
+f = open("part1results/results3_2_changelearningrate.txt", 'w')
+f.write('\nchanging learningrate\n')
+
+learningrates = np.array([0.1,0.2,0.3,0.4,0.5,0.6, 0.8])
+mses=np.zeros(learningrates.shape)
 
 
-for i in range(len(no_of_units)):
-    network = RBFNetwork(n_inputs=1, n_rbf=no_of_units[i], n_outputs=1, rbf_var=0.2)
+for i in range(len(learningrates)):
+    network = RBFNetwork(n_inputs=1, n_rbf=40, n_outputs=1, rbf_var=0.1, learning_rate=learningrates[i] )
     x_train = np.arange(0, 2*np.pi, 0.1)
 
     y = np.zeros(x_train.shape)
@@ -39,126 +40,11 @@ for i in range(len(no_of_units)):
     x_test = add_noise(x_test, 0.1)
 
 
-    network.train_sequential_delta(x_train, y_train_target)
+    mses = network.train_sequential_delta(x_train, y_train_target)
 
     mse = network.test_sequential_delta(x_test, y_test_target)
-    mses[i] = mse
-    f.write('no. of units: '+ str(no_of_units[i]) + '  mse: ' + str(mses[i]) + '\n')
 
-
-
-f.write('\nwidth=0.2\n')
-no_of_units = np.array([1,2,3,4,5,6,7,8,9,10,15,20,25,30,40,50,100])
-mses=np.zeros(no_of_units.shape)
-
-
-for i in range(len(no_of_units)):
-    network2 = RBFNetwork(n_inputs=1, n_rbf=no_of_units[i], n_outputs=1, rbf_var=0.2)
-    x_train = np.arange(0, 2*np.pi, 0.1)
-
-    y = np.zeros(x_train.shape)
-    y_train_target = list(map(sin2, x_train))
-
-    x_train = add_noise(x_train, 0.1)
-
-    x_test = np.arange(0.05, 2*np.pi, 0.1)
-
-
-    y_test_target = list(map(sin2, x_test))
-    x_test = add_noise(x_test, 0.1)
-
-
-    network2.train_sequential_delta(x_train, y_train_target)
-
-    mse = network2.test_sequential_delta(x_test, y_test_target)
-    mses[i] = mse
-    f.write('no. of units: '+ str(no_of_units[i]) + '  mse: ' + str(mses[i]) + '\n')
-
-
-f.write('\nwidth=0.3\n')
-no_of_units = np.array([1,2,3,4,5,6,7,8,9,10,15,20,25,30,40,50,100])
-mses=np.zeros(no_of_units.shape)
-
-
-for i in range(len(no_of_units)):
-    network3 = RBFNetwork(n_inputs=1, n_rbf=no_of_units[i], n_outputs=1, rbf_var=0.3)
-    x_train = np.arange(0, 2*np.pi, 0.1)
-
-    y = np.zeros(x_train.shape)
-    y_train_target = list(map(sin2, x_train))
-
-    x_train = add_noise(x_train, 0.1)
-
-    x_test = np.arange(0.05, 2*np.pi, 0.1)
-
-
-    y_test_target = list(map(sin2, x_test))
-    x_test = add_noise(x_test, 0.1)
-
-
-    network3.train_sequential_delta(x_train, y_train_target)
-
-    mse = network3.test_sequential_delta(x_test, y_test_target)
-    mses[i] = mse
-    f.write('no. of units: '+ str(no_of_units[i]) + '  mse: ' + str(mses[i]) + '\n')
-
-
-
-f.write('\nwidth=0.4\n')
-no_of_units = np.array([1,2,3,4,5,6,7,8,9,10,15,20,25,30,40,50,100])
-mses=np.zeros(no_of_units.shape)
-
-
-for i in range(len(no_of_units)):
-    network4 = RBFNetwork(n_inputs=1, n_rbf=no_of_units[i], n_outputs=1, rbf_var=0.4)
-    x_train = np.arange(0, 2*np.pi, 0.1)
-
-    y = np.zeros(x_train.shape)
-    y_train_target = list(map(sin2, x_train))
-
-    x_train = add_noise(x_train, 0.1)
-
-    x_test = np.arange(0.05, 2*np.pi, 0.1)
-
-
-    y_test_target = list(map(sin2, x_test))
-    x_test = add_noise(x_test, 0.1)
-
-
-    network4.train_sequential_delta(x_train, y_train_target)
-
-    mse = network4.test_sequential_delta(x_test, y_test_target)
-    mses[i] = mse
-    f.write('no. of units: '+ str(no_of_units[i]) + '  mse: ' + str(mses[i]) + '\n')
-
-
-f.write('\nwidth=0.6\n')
-no_of_units = np.array([1,2,3,4,5,6,7,8,9,10,15,20,25,30,40,50,100])
-mses=np.zeros(no_of_units.shape)
-
-
-for i in range(len(no_of_units)):
-    network5 = RBFNetwork(n_inputs=1, n_rbf=no_of_units[i], n_outputs=1, rbf_var=0.6)
-    x_train = np.arange(0, 2*np.pi, 0.1)
-
-    y = np.zeros(x_train.shape)
-    y_train_target = list(map(sin2, x_train))
-
-    x_train = add_noise(x_train, 0.1)
-
-    x_test = np.arange(0.05, 2*np.pi, 0.1)
-
-
-    y_test_target = list(map(sin2, x_test))
-    x_test = add_noise(x_test, 0.1)
-
-
-    network5.train_sequential_delta(x_train, y_train_target)
-
-    mse = network5.test_sequential_delta(x_test, y_test_target)
-    mses[i] = mse
-    f.write('no. of units: '+ str(no_of_units[i]) + '  mse: ' + str(mses[i]) + '\n')
-
+    f.write('learning rate: '+ str(learningrates[i]) + '  mses: ' + str(mses) + '\n')
 
 
 
