86be8f60d9f04655217552f2f431e5247b585583
paddyflan <flanagp9@tcd.ie>
2022-03-01 15:17:14 +0100

    noise

--- 2-RBFs-CL-SOMs/3_2.py
+++ 2-RBFs-CL-SOMs/3_2.py
@@ -17,8 +17,8 @@ def add_noise(data, variance):
     return data
 
 
-f = open("part1results/results3_2_positioning_RBFs_effect.txt", 'w')
-f.write('\nwe positioned them evenly over interval 0.05 to 2pi, after 20 epochs the mse on testset is\n')
+f = open("part1results/results3_2_comparing_effect_of_noise.txt", 'w')
+f.write('\nwth noise after 20 epochs, mse on testset is\n')
 
 
 network = RBFNetwork(n_inputs=1, n_rbf=40, n_outputs=1, rbf_var=0.1 )
@@ -44,21 +44,21 @@ f.write('mse:' + str(mse) + '\n')
 
 
 
-f.write('\nwhen positioned randomly over interval 0.05 to 2pi after 2p epochs the mse on testset is \n')
+f.write('\nwithout noise, after 20 epochs mse on testset is\n')
 
-network2 = RBFNetwork(n_inputs=1, n_rbf=40, n_outputs=1, rbf_var=0.1, rbf_positioning='random' )
+network2 = RBFNetwork(n_inputs=1, n_rbf=40, n_outputs=1, rbf_var=0.1)
 x_train = np.arange(0, 2*np.pi, 0.1)
 
 y = np.zeros(x_train.shape)
 y_train_target = list(map(sin2, x_train))
 
-x_train = add_noise(x_train, 0.1)
+
 
 x_test = np.arange(0.05, 2*np.pi, 0.1)
 
 
 y_test_target = list(map(sin2, x_test))
-x_test = add_noise(x_test, 0.1)
+
 
 
 mses = network2.train_sequential_delta(x_train, y_train_target)
